1fc635bbf8842bc222683b31499550a8ba90631f
Waleed Alenazi <waleedf112@gmail.com>
2022-12-16 23:46:02 +0300

    implemented usecases

---
+++ feture/hooks/pre_gen.dart
@@ -0,0 +1,11 @@
+import 'package:mason/mason.dart';
+
+void run(HookContext context) {
+  final vars = context.vars;
+  for (int i = 0; i < vars['usecases'].length; i++) {
+    vars['usecases'][i]['has_params'] = vars['usecases'][i]['params']?.isNotEmpty ?? false;
+    vars['usecases'][i]['return'] ??= 'void';
+    vars['usecases'][i]['stream'] ??= false;
+  }
+  context.vars = vars;
+}
