c35de5e891c30de60a2ae1393c6a663d5a120e32
Waleed Alenazi <waleedf112@gmail.com>
2022-12-16 23:45:48 +0300

    implemented injection container

---
+++ feture/__brick__/dependency_injection/{{feture_name.snakeCase()}}_injection_container.dart
@@ -0,0 +1,29 @@
+{{#datasources_names}}import '../../data/datasources/{{feture_name.snakeCase()}}_{{datasource_name.snakeCase()}}_data_source.dart';
+{{/datasources_names}}
+import '../../data/repositories/{{feture_name.snakeCase()}}_repository_impl.dart';
+import '../../domain/repositories/{{feture_name.snakeCase()}}_repository.dart';
+import '../../domain/usecases/{{feture_name.snakeCase()}}/{{feture_name.snakeCase()}}_usecases.dart';
+import '../../presentation/blocs/{{feture_name.snakeCase()}}/{{feture_name.snakeCase()}}_bloc.dart';
+import 'dependency_injection.dart';
+
+abstract class {{feture_name1.pascalCase()}}InjectionContainer {
+  static void init() {
+    sl.registerLazySingleton<{{feture_name.pascalCase()}}Bloc>(
+      () => {{feture_name.pascalCase()}}Bloc({{#usecases}}
+        {{usecase_name.camelCase()}}{{feture_name.pascalCase()}}Usecase: sl(),{{/usecases}}
+      ),
+    );
+    sl.registerLazySingleton<{{feture_name.pascalCase()}}Repository>(
+      () => {{feture_name.pascalCase()}}RepositoryImpl({{#datasources}}
+        {{datasource_name.camelCase()}}DataSource: sl(),{{/datasources}}
+      ),
+    );
+    {{#datasources}}
+    sl.registerLazySingleton<{{feture_name.pascalCase()}}{{datasource_name.pascalCase()}}DataSource>(
+      () => {{feture_name.pascalCase()}}{{datasource_name.pascalCase()}}DataSourceImpl(),
+    );{{/datasources}}
+
+    {{#usecases}}sl.registerLazySingleton<{{usecase_name.pascalCase()}}{{feture_name.pascalCase()}}Usecase>(() => {{usecase_name.pascalCase()}}{{feture_name.pascalCase()}}Usecase(repository: sl()));
+    {{/usecases}}
+  }
+}
