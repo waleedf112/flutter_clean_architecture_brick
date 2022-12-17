075b7926ab3e3ad8e57f2c85985ecbfec1817542
Waleed Alenazi <waleedf112@gmail.com>
2022-12-17 03:12:54 +0300

    implemented data sources

---
+++ feture/__brick__/data/datasources/{{feture_name.snakeCase()}}_{{datasource.snakeCase()}}_data_source.dart
@@ -0,0 +1,22 @@
+import '../../core/dependency_injection/dependency_injection.dart';
+import '../../core/error/failure.dart';
+import '../models/{{feture_name.snakeCase()}}_model.dart';
+
+abstract class {{feture_name.pascalCase()}}{{datasource.pascalCase()}}DataSource {
+  {{#usecases}}{{#stream}}Stream{{/stream}}{{^stream}}Future{{/stream}}<{{return_model}}> {{usecase_name.camelCase()}}({{#has_params}}{{usecase_name.pascalCase()}}{{feture_name.pascalCase()}}Params params{{/has_params}});
+  {{/usecases}}
+}
+
+class {{feture_name.pascalCase()}}{{datasource.pascalCase()}}DataSourceImpl implements {{feture_name.pascalCase()}}{{datasource.pascalCase()}}DataSource {
+  {{#usecases}}{{#stream}}
+  @override
+  Stream<{{return_model}}> {{usecase_name.camelCase()}}({{#has_params}}{{usecase_name.pascalCase()}}{{feture_name.pascalCase()}}Params params{{/has_params}}) async* {
+    yield* stream.asyncExpand<{{return_model}}>((result) async* {
+      //TODO: implement {{usecase_name}}
+    });
+  }{{/stream}}{{^stream}}
+  @override
+  Future<{{return_model}}> {{usecase_name.camelCase()}}({{#has_params}}{{usecase_name.pascalCase()}}{{feture_name.pascalCase()}}Params params{{/has_params}}) async {
+    //TODO: implement {{usecase_name}}
+  }{{/stream}}{{/usecases}}
+}
--- feture/hooks/pre_gen.dart
+++ feture/hooks/pre_gen.dart
@@ -5,6 +5,7 @@ void run(HookContext context) {
   for (int i = 0; i < vars['usecases'].length; i++) {
     vars['usecases'][i]['has_params'] = vars['usecases'][i]['params']?.isNotEmpty ?? false;
     vars['usecases'][i]['return'] ??= 'void';
+    vars['usecases'][i]['return_model'] ??= 'void';
     vars['usecases'][i]['stream'] ??= false;
   }
   context.vars = vars;
